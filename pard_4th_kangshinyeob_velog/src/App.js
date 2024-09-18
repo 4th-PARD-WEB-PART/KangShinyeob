25907914767f5719dbb58ed695c482d50d4a4710
yeob <happyk100@naver.com>
2024-09-19 00:13:43 +0900

    update: input 태그에 name 지정

--- pard_4th_kangshinyeob_velog/src/App.js
+++ pard_4th_kangshinyeob_velog/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import styled from "styled-components";
-import { BaseContainer, H1, Hp, TextInput, InputContainer, TextLabel, Section, CheckBoxInput, Link, CheckBoxLabel, CheckBoxContainer } from './Pages/RegisterPage/RegisterPage';
+import { BaseContainer, H1, Hp, TextInput, InputContainer, TextLabel, Section, CheckBoxInput, Link, CheckBoxLabel, CheckBoxContainer, ButtonContainer, Button } from './Pages/RegisterPage/RegisterPage';
 
 function App() {
   return (
@@ -15,28 +15,31 @@ function App() {
         <Section id="input-section">
           <form>
             <InputContainer>
-              <TextInput id="input-name" type="text" width="249px" placeholder="이름을 입력해주세요." />
+              <TextInput id="input-name" name="input-name" type="text" width="249px" placeholder="이름을 입력해주세요." />
               <TextLabel for="input-name">이름</TextLabel>
             </InputContainer>
             <InputContainer>
-              <TextInput id="input-email" type="text" width="333px" placeholder="이메일을 입력해주세요." />
+              <TextInput id="input-email" name="input-email" type="text" width="333px" placeholder="이메일을 입력해주세요." />
               <TextLabel for="input-email">이메일</TextLabel>
             </InputContainer>
             <InputContainer>
-              <TextInput id="input-pw" type="text" width="274px" placeholder="비밀번호를 입력해주세요." />
+              <TextInput id="input-pw" name="input-pw" type="text" width="274px" placeholder="비밀번호를 입력해주세요." />
               <TextLabel for="input-pw">비밀번호</TextLabel>
             </InputContainer>
             <InputContainer>
-              <TextInput id="input-introduce" type="text" width="373px" placeholder="당신을 한 줄로 소개해보세요" />
+              <TextInput id="input-introduce" name="input-introduce" type="text" width="373px" placeholder="당신을 한 줄로 소개해보세요" />
               <TextLabel for="input-introduce">한 줄 소개</TextLabel>
             </InputContainer>
 
             <CheckBoxContainer>
-              <CheckBoxInput id="input-agreement" type="checkbox"/>
+              <CheckBoxInput id="input-agreement" name="input-agreement" type="checkbox"/>
               <CheckBoxLabel for="input-agreement"><Link href="#">이용약관</Link>과 <Link href="#">개인정보취급방침</Link>에 동의합니다.</CheckBoxLabel>
             </CheckBoxContainer>
 
-            
+            <ButtonContainer>
+              <Button type="reset" bg_color="#DEE2E6" t_color="#000000">취소</Button>
+              <Button type="submit" bg_color="#15B886" t_color="#FFFFFF">가입</Button>
+            </ButtonContainer>
           </form>
         </Section> {/* End of #input-section */}
       </Section>
