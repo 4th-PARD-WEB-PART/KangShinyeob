cfd075ad9c3698d7359f6d8b9eadbd1c146067be
yeob <happyk100@naver.com>
2024-10-07 01:09:53 +0900

    update: recoil 사용 세팅

--- pard_4th_kangshinyeob_velog/src/index.js
+++ pard_4th_kangshinyeob_velog/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
